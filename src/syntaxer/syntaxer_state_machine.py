392eee490df36701633039174e788953509ba7bd
Dmitriy <mr.priskoka@yandex.com>
2020-04-15 12:50:58 +0300

    Type hints added.

--- src/syntaxer/syntaxer_state_machine.py
+++ src/syntaxer/syntaxer_state_machine.py
@@ -1,12 +1,17 @@
 from lexer.state_machine import State, StateMachine
+from typing import Dict, Callable
+from lexer.token import Token
 
 
 class SyntaxerStateMachine(StateMachine):
-    def __init__(self, name, success_state, rules):
+    def __init__(self,
+                 name,
+                 success_state: State,
+                 rules: Dict[State, Callable[[Token], State]]):
         super(SyntaxerStateMachine, self).__init__(name, rules)
         self.successState = success_state
 
-    def process_object(self, obj):
+    def process_object(self, obj: Token):
         self.prevState = self.state
         if self.state != State.undefined:
             self.state = self.rules[self.prevState](obj)
