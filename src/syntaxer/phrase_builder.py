f260c4c2da6e4a5b5568e44acdfccd38aa71443a
Dmitriy <mr.priskoka@yandex.com>
2020-04-16 22:05:44 +0300

    Get context return value change.

--- src/syntaxer/phrase_builder.py
+++ src/syntaxer/phrase_builder.py
@@ -11,7 +11,7 @@ class PhraseBuildError(Exception):
         self.line = line
 
 
-def phrase_builder(context: PhraseSubclass, phrase_class: PhraseClass, temp_phrase: List[Token], phrase_line: int) -> Phrase:
+def phrase_builder(context: Phrase, phrase_class: PhraseClass, temp_phrase: List[Token], phrase_line: int) -> Phrase:
     phrase = Phrase(phrase_class)
     if phrase_class == PhraseClass.block:
         build_block(phrase, context, temp_phrase, phrase_line)
@@ -29,7 +29,7 @@ def phrase_builder(context: PhraseSubclass, phrase_class: PhraseClass, temp_phra
     return phrase
 
 
-def build_block(phrase: Phrase, context: PhraseSubclass, temp_phrase: List[Token], phrase_line: int):
+def build_block(phrase: Phrase, context: Phrase, temp_phrase: List[Token], phrase_line: int):
     if temp_phrase[0].token_class == TokenClass.word:
         phrase.keyword = temp_phrase[0]
     else:
@@ -37,19 +37,19 @@ def build_block(phrase: Phrase, context: PhraseSubclass, temp_phrase: List[Token
                                f"Unexpected phrase sequence.", phrase_line)
 
     if phrase.keyword.value == "main":
-        if context == PhraseSubclass.program:
+        if context.phrase_subclass == PhraseSubclass.program:
             phrase.phrase_subclass = PhraseSubclass.body
         else:
             raise PhraseBuildError(f"Phrase build error at line {phrase_line}.\n"
-                                   f"Main block can not be defined inside {context.name}.", phrase_line)
-    elif context == PhraseSubclass.program:
+                                   f"Main block can not be defined inside {context.phrase_subclass.name}.", phrase_line)
+    elif context.phrase_subclass == PhraseSubclass.program:
         phrase.phrase_subclass = PhraseSubclass.expression
-    elif context == PhraseSubclass.body or context == PhraseSubclass.expression:
+    elif context.phrase_subclass == PhraseSubclass.body or context == PhraseSubclass.expression:
         phrase.phrase_subclass = PhraseSubclass.device
 
 
-def build_operator(phrase: Phrase, context: PhraseSubclass, temp_phrase: List[Token], phrase_line):
-    if context != PhraseSubclass.program:
+def build_operator(phrase: Phrase, context: Phrase, temp_phrase: List[Token], phrase_line):
+    if context.phrase_subclass != PhraseSubclass.program:
         if temp_phrase[0].token_class == TokenClass.word:
             phrase.keyword = temp_phrase[0]
         else:
@@ -67,8 +67,8 @@ def build_comment(phrase: Phrase, temp_phrase: List[Token]):
     phrase.params = temp_phrase
 
 
-def build_label(phrase: Phrase, context: PhraseSubclass, temp_phrase: List[Token], phrase_line):
-    if context != PhraseSubclass.program:
+def build_label(phrase: Phrase, context: Phrase, temp_phrase: List[Token], phrase_line):
+    if context.phrase_subclass != PhraseSubclass.program:
         if temp_phrase[0].token_class == TokenClass.word:
             phrase.keyword = temp_phrase[0]
         else:
