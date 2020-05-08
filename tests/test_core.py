255c578e81a77ca5332a46d3b6899443ac1ffef7
Dmitriy <mr.priskoka@yandex.com>
2020-05-08 11:41:04 +0300

    Added phrase_builder test.

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,9 +1,15 @@
 from module_test import ModuleTest
 
+import test_phrase_builder
 import test_lang_dict
 import test_state_machine
 import test_lexer
 
-test_chain = [test_state_machine, test_lexer, test_lang_dict]
+test_chain = [
+    test_state_machine,
+    test_lexer,
+    test_lang_dict,
+    test_phrase_builder
+]
 tests = ModuleTest(verbosity=0, verbose_result=True)
 tests.test_chain(test_chain)
