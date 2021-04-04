6605ba813431947e56d001c12c7142503b4e4c9c
Dmitriy <mr.priskoka@yandex.com>
2021-04-04 20:21:24 +0300

    Added rule parser.

--- src/lr_parser/parser_gen/GrammarParser.py
+++ src/lr_parser/parser_gen/GrammarParser.py
@@ -1,6 +1,15 @@
 from pathlib import Path
 from lr_parser.RuleTable import Rule
+from lexer.token import TokenClass
 import sys
+
+
+class NonTerminal:
+    def __init__(self, name: str):
+        self.name: str = name
+
+    def __repr__(self):
+        return self.name
 
 
 class GrammarParser:
@@ -8,6 +17,17 @@ class GrammarParser:
         self.terminals_list = set()
         self.nonterminals_list = set()
         self.rules: list[Rule] = list()
+
+        self.grammar_terminals = [
+            TokenClass.word,
+            TokenClass.colon,
+            TokenClass.b_op,
+            TokenClass.b_cl,
+            TokenClass.hash,
+            TokenClass.cb_cl,
+            TokenClass.cb_op,
+            TokenClass.newline
+        ]
 
     def generate_from_file(self, path: Path):
         if not path.exists():
@@ -21,6 +41,7 @@ class GrammarParser:
                     self._parse_rule(line)
         print(self.terminals_list)
         print(self.nonterminals_list)
+        self.print_rules()
 
     def generate_from_str(self, grammar: str):
         for line in grammar.splitlines():
@@ -29,6 +50,7 @@ class GrammarParser:
                 self._parse_rule(line)
         print(self.terminals_list)
         print(self.nonterminals_list)
+        self.print_rules()
 
     def _parse_rule(self, line: str):
         production = line.split("->")
@@ -43,8 +65,30 @@ class GrammarParser:
 
         forms = production[1].split("|")
         for form in forms:
-            terminals_list = form.split(" ")
-            for terminal in terminals_list:
-                terminal = terminal.strip()
-                if terminal.islower():
-                    self.terminals_list.add(terminal)
+            raw_terminals = form.split(" ")
+            production_body = list()
+            for body_item in raw_terminals:
+                if not body_item:
+                    continue
+
+                body_item = body_item.strip()
+                if body_item.islower():
+                    term = TokenClass.from_str(body_item)
+                    if term not in self.grammar_terminals:
+                        raise Exception(f"Unknown terminal {body_item}.")
+
+                    production_body.append(term)
+                    self.terminals_list.add(term)
+                else:
+                    production_body.append(self.get_non_terminal(body_item))
+            self.rules.append(self.get_rule(production[0], production_body))
+
+    def get_rule(self, production_head: str, production_body: list[TokenClass]) -> Rule:
+        return Rule(production_head, production_body)
+
+    def get_non_terminal(self, name: str) -> NonTerminal:
+        return NonTerminal(name)
+
+    def print_rules(self):
+        for rule in self.rules:
+            print(rule)
