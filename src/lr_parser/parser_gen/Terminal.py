ff253e1486278c4d03ef9024a8f89dfff6b147fc
Dmitriy <mr.priskoka@yandex.com>
2021-04-10 18:24:12 +0300

    Add epsilon and eof constants.

--- src/lr_parser/parser_gen/Terminal.py
+++ src/lr_parser/parser_gen/Terminal.py
@@ -2,8 +2,19 @@ from lr_parser.parser_gen.ProductionItem import ProductionItem, ProductionItemTy
 
 
 class Terminal(ProductionItem):
+    EPSILON = "epsilon"
+    EOF = "end_of_file"
+
     def __init__(self, name: str):
         super().__init__(name, ProductionItemType.TERMINAL_TYPE)
 
     def __repr__(self):
         return self.item_name
+
+    @staticmethod
+    def get_epsilon():
+        return Terminal(Terminal.EPSILON)
+
+    @staticmethod
+    def get_eof():
+        return Terminal(Terminal.EOF)
